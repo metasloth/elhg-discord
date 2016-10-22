b77fd5302728da0be25884f0604c84dd832f0bbb
metasloth <th3adm1ral@gmail.com>
2016-10-22 01:03:54 -0500

    Added chat logging and repost filters

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const cheerio = require('cheerio')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const secret = require('./secret.json')
+
+let botlog
+let staleMemes = []
 const airhornCommands = [
   '!airhorn',
   '!anotha',
@@ -29,25 +32,30 @@ function getMeme () {
         console.log(error)
       } else {
         let $ = cheerio.load(body)
-        let topPost = $("[data-rank='1']").attr('data-url')
+        let meme = $("[data-rank='1']").attr('data-url')
+        // Check for unique post
+        for (let i = 2; staleMemes.indexOf(meme) > -1; i++) {
+          meme = $("[data-rank='" + i + "']").attr('data-url')
+        }
+        staleMemes.push(meme)
         // Make sure imgur single image links are direct 
-        if (topPost.includes('imgur') && !topPost.includes('.', topPost.length - 5)
-          && !topPost.includes('/gallery/') && !topPost.includes('/a/')) {
-          request(topPost, (error, response, body) => {
-            // if the imgur request fails return the link as is
+        if (meme.includes('imgur') && !meme.includes('.', meme.length - 5)
+          && !meme.includes('/gallery/') && !meme.includes('/a/')) {
+          request(meme, (error, response, body) => {
+            // If the imgur request fails return the link as is
             if (error) {
-              resolve(topPost)
+              resolve(meme)
               console.log(error)
             } else {
-              // look for a gifv url, otherwise append .jpg to the link
+              // Look for a gifv url, otherwise append .jpg to the link
               let $ = cheerio.load(body)
               let gifv = $("[itemprop='embedURL']").attr('content')
-              topPost = (gifv != null) ? gifv : (topPost + '.jpg')
-              resolve(topPost)
+              meme = (gifv != null) ? gifv : (meme + '.jpg')
+              resolve(meme)
             }
           })
         } else {
-          resolve(topPost)
+          resolve(meme)
         }
       }
     })
@@ -59,23 +67,28 @@ client.on('message', msg => {
   if (msg.content.toLowerCase() === '!meme') {
     getMeme().then(response => {
       msg.reply("here's the spiciest meme of the hour fam: " + response)
-      console.log(new Date().toLocaleTimeString() + ` Sent "${response}" to ${msg.author.username}`)
+      botlog.sendMessage(` Sent "${response}" to ${msg.author.username}`)
     }, error => {
       msg.reply("I can't get memes right now homie")
     })
   } else if (airhornCommands.indexOf(msg.content.toLowerCase()) > -1) {
     msg.delete(2000)
-    console.log(new Date().toLocaleTimeString() + ` Caught Commmand: ${msg.content} from ${msg.author.username}`)
+    botlog.sendMessage(`Removed commmand "${msg.content}" from ${msg.author.username}`)
   }
 })
 
 client.on('ready', () => {
   client.user.setStatus('online', 'games on linux kappa')
-  console.log(new Date().toLocaleTimeString() + " bot_irl is ready to meme!")
+  console.log(new Date() + ': bot_irl is ready to meme!')
+  botlog = client.channels.find('id', secret.botLogChannel)
 })
 
-client.on('disconnected', () => {
-  console.log(new Date().toLocaleTimeString() + ' Disconnected!')
+client.on('disconnect', () => {
+  console.log(new Date() + ': Disconnected!')
+})
+
+client.on('error', (error) => {
+  console.log(new Date() + error)
 })
 
 client.login(secret.token)
